cb6107cbbb2488f749b653b4da76fd2ef47fc4af
Isabella <isabellarose.hernandez01@gmail.com>
2022-03-30 21:02:11 -0500

    working on servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,10 +3,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        response.setContentType("text/html");
+
+        String outputResp = "<h1>Hello, World!</h1>";
+
+        if(request.getParameter("name") != null) {
+            outputResp = "<h1> Hello " + request.getParameter("name") + "!</h1>";
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println(outputResp);
+
     }
 }
