ff067230f51b5730438e3e8c7237b6556b923996
Isabella <isabellarose.hernandez01@gmail.com>
2022-03-30 21:10:24 -0500

    working on servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,15 +3,19 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
-    }
-// change to check if branch was made
 
+        String outputResp = "<h1>Hello, World!</h1>";
+
+        if(request.getParameter("name") != null) {
+            outputResp = "<h1> Hello " + request.getParameter("name") + "!</h1>";
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println(outputResp);
+
+    }
 }
